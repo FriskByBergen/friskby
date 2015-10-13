16e36db227f6f3268dc3aa95641137d1c29e7741
Joakim Hove <joakim.hove@gmail.com>
2015-10-13 21:48:43 +0200

    fixup! Initial sensor api commit

--- sensor/admin.py
+++ sensor/admin.py
@@ -4,4 +4,6 @@ from models import *
 admin.site.register(SensorID)
 admin.site.register(Company)
 admin.site.register(DeviceType)
+admin.site.register(MeasurementType)
+admin.site.register(Location)
 

28724d3524e5322f0cf0cc23765b6798f8583ee6
Joakim Hove <joakim.hove@gmail.com>
2015-10-18 22:50:00 +0200

    SensorID is not (as) hardcoded

--- friskby/GW/randomGW.py
+++ friskby/GW/randomGW.py
@@ -6,10 +6,11 @@ import time
 from friskby_gw import FriskByGW
 
 
-gw = FriskByGW( )
-sensor = gw.getSensor("RANDOM")
+gw = FriskByGW(  )
+sensor_id = "RANDOM"
+sensor = gw.getSensor( sensor_id )
 if sensor is None:
-    msg = "Sorry - the friskby server at:%s does not have a %s sensor - add that manually first." % (gw.getRootURL() , "RANDOM")
+    msg = "Sorry - the friskby server at:%s does not have a %s sensor - add that manually first." % (gw.getRootURL() , sensor_id)
     sys.exit( msg )
 
 
@@ -19,7 +20,7 @@ sleep_time = 1
 corr = 0.90
 
 try:
-    last_value = sensor.getLastValue( )
+    last_value = sensor.getLastValue( )[1]
 except Exception:
     last_value = 0.50 * (min_value + max_value)
 

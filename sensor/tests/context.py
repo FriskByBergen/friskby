c21c2dc4291fc4b89e22c9282646fe6b1aab6dab
Joakim Hove <joakim.hove@gmail.com>
2015-10-28 08:23:38 +0100

    Created module context with testContext

---
+++ sensor/tests/context.py
@@ -0,0 +1,36 @@
+from sensor.models import *
+
+class TestContext(object):
+    def __init__(self):
+        self.loc = Location.objects.create( name = "Ulriken" , latitude = 200 , longitude = 120 , altitude = 600)
+        self.hp = Company.objects.create( name = "Hewlett Packard" )
+        self.dev = DeviceType.objects.create( name = "HP-X123" , company = self.hp)
+        self.mtype = MeasurementType.objects.create( name = "Temperature" )
+        self.raw_data = DataType.objects.get( pk = "RAWDATA" )
+        self.test_data = DataType.objects.get( pk = "TEST" )
+
+        self.temp_sensor = SensorID.objects.create( id = "TEMP:XX",
+                                                    location = self.loc,
+                                                    parent_device = self.dev,
+                                                    measurement_type = self.mtype,
+                                                    description = "Measurement of temperature",
+                                                    unit = "Degree celcius",
+                                                    min_value = 0,
+                                                    max_value = 100)
+
+        self.hum_sensor = SensorID.objects.create( id = "HUM:XX",
+                                                   parent_device = self.dev,
+                                                   measurement_type = self.mtype,
+                                                   description = "Measurement humidity",
+                                                   unit = "Percent humidity",
+                                                   min_value = 0,
+                                                   max_value = 100,
+                                                   data_type = self.raw_data )
+
+        self.ts = TimeStamp.objects.create( timestamp = TimeStamp.parse_datetime("2015-10-10T10:10:00+01") )
+        self.data_info1 = DataInfo.objects.create( timestamp = self.ts, 
+                                                   location = self.loc,
+                                                   sensor = self.hum_sensor )
+
+        self.data_info2 = DataInfo.objects.create( timestamp = self.ts, 
+                                                   sensor = self.temp_sensor )

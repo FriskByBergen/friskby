a7fae9b41f71de6b681acb1bcfc011ef2235fc19
Joakim Hove <joakim.hove@gmail.com>
2015-10-30 06:27:07 +0100

    Added CORS headers

--- friskby/GW/randomGW.py
+++ friskby/GW/randomGW.py
@@ -13,11 +13,14 @@ if sensor is None:
     msg = "Sorry - the friskby server at:%s does not have a %s sensor - add that manually first." % (gw.getRootURL() , sensor_id)
     sys.exit( msg )
 
-
+num = 1
 min_value = sensor.getMinValue( )
 max_value = sensor.getMaxValue( )
 sleep_time = 1
 corr = 0.90
+
+if len(sys.argv) > 1:
+    num = int(sys.argv[1])
 
 try:
     last_value = sensor.getLastValue( )[1]
@@ -25,13 +28,15 @@ except Exception:
     last_value = 0.50 * (min_value + max_value)
 
 
-random_value = (max_value - min_value) * (0.5 + 1.25 * (random.random() - 0.5))
-new_value = last_value * corr + (1 - corr) * random_value 
+for i in range(num):
+    random_value = (max_value - min_value) * (0.5 + 1.25 * (random.random() - 0.5))
+    new_value = last_value * corr + (1 - corr) * random_value 
 
-if new_value > max_value:
-    new_value = max_value
+    if new_value > max_value:
+        new_value = max_value
     
-if new_value < min_value:
-    new_value = min_value
+    if new_value < min_value:
+        new_value = min_value
 
-sensor.postValue( new_value )
+    sensor.postValue( new_value )
+    last_value = new_value

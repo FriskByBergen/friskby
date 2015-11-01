aa9c07faecdab30be1cf909e0f35e2380e8a687a
Joakim Hove <joakim.hove@gmail.com>
2015-11-01 07:46:30 +0100

    Added debug toolbar to installed_apps

--- friskby/settings/__init__.py
+++ friskby/settings/__init__.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'corsheaders',
     'rest_framework',
     'api_key',

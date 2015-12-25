86a623db42508a234b81fc6375e6e4917d77b5d4
Joakim Hove <joakim.hove@gmail.com>
2015-12-25 23:56:27 +0100

    Fixed update_rawdata to check for range error

--- sensor/bin/update_rawdata.py
+++ sensor/bin/update_rawdata.py
@@ -15,23 +15,30 @@ def make_qs(offset , size):
     print "Range [%d , %d)" % (offset , last_id)
     return RawData.objects.filter(id__gte = offset, id__lt = last_id) 
 
-qs_size = 10000
+qs_size = 2000
 offset = 0
 
+sensor_map = {}
+for sensor in Sensor.objects.all():
+    sensor_map[sensor.id] = sensor
+
 while offset < 2000000:
-    
     qs = make_qs( offset , qs_size)
     for rd in qs:
-        if rd.value == -1:
-            if rd.status in [RawData.RAWDATA , RawData.PROCESSED]:
-                try:
-                    rd.value = float(rd.string_value)
-                    rd.string_value = None
+        if rd.status in [RawData.RAWDATA , RawData.PROCESSED]:
+            try:
+                rd.value = float(rd.string_value)
+
+                rd.string_value = None
+                if sensor.sensor_type.valid_range( rd.value ):
                     rd.status = RawData.RAWDATA
-                except ValueError:
-                    rd.status = RawData.FORMAT_ERROR
-                    print "\nFormat error: %d" % rd.id
+                else:
+                    rd.status = RawData.RANGE_ERROR
+                    print "\nRange error: %d" % rd.id
+            except ValueError:
+                rd.status = RawData.FORMAT_ERROR
+                print "\nFormat error: %d" % rd.id
         
-                rd.save()
+            rd.save()
 
     offset += qs_size            

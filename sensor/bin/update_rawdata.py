fac124ba5a3da822658a065e360c0e7921870a3c
Joakim Hove <joakim.hove@gmail.com>
2016-01-07 07:37:26 +0100

    Update range

--- sensor/bin/update_rawdata.py
+++ sensor/bin/update_rawdata.py
@@ -16,13 +16,13 @@ def make_qs(offset , size):
     return RawData.objects.filter(id__gte = offset, id__lt = last_id) 
 
 qs_size = 2000
-offset = 0
+offset = 2000000
 
 sensor_map = {}
 for sensor in Sensor.objects.all():
     sensor_map[sensor.id] = sensor
 
-while offset < 2000000:
+while offset < 3000000:
     qs = make_qs( offset , qs_size)
     for rd in qs:
         if rd.status in [RawData.RAWDATA , RawData.PROCESSED]:

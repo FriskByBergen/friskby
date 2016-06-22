68dada41071de177e6e088e0ff9a3f8e70fa09d1
Joakim Hove <joakim.hove@gmail.com>
2016-06-22 19:59:38 +0200

    Minor

--- sensor/models.py
+++ sensor/models.py
@@ -256,7 +256,7 @@ class Device( Model ):
     def clientConfig(self):
         config = {"post_key" : str(self.post_key.external_key),
                   "sensor_list" : [ sensor.id for sensor in self.sensorList() ],
-                  "post_url" : reverse("sensor-post")}
+                  "post_path" : reverse("sensor-post")}
         
         if self.git_version:
             config["git_repo"] = self.git_version.repo

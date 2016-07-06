cd33d24f761d4458e502dfd2b6eb2abcc8230331
Joakim Hove <joakim.hove@gmail.com>
2016-07-06 20:51:49 +0200

    Added DevicePlot model

--- plot/models.py
+++ plot/models.py
@@ -4,13 +4,18 @@ import pytz
 import datetime
 from django.db.models import *
 from pythoncall.models import * 
+from sensor.models import *
 
+from . import lib
 
 class Plot(Model):
     name = CharField( max_length = 32 )
     description = CharField( max_length = 256 )
     tag = CharField( max_length = 128 )
-    python_callable = ForeignKey( PythonCall )
+
+    # Derived class can implement plotting inline in the updatePlot( )
+    # method, therefor this can be null and void.
+    python_callable = ForeignKey( PythonCall , blank = True , null = True) 
     html_code = TextField( blank = True )
     last_update = DateTimeField( blank = True)
 
@@ -20,6 +25,9 @@ class Plot(Model):
         
     def updatePlot(self):
         try:
+            if self.python_callable is None:
+                raise ValidationError("No callable")
+
             func = self.python_callable.getCallable( )
         except:
             raise ValidationError("Import python callable:%s failed" % self.python_callable)
@@ -45,4 +53,64 @@ class Plot(Model):
             self.updatePlot( )
         else:
             super(Plot , self).save( *args , **kwargs )
+
+
+
+class DevicePlot(Plot):
+    device = ForeignKey( Device )
+    duration = DurationField( blank = True , null = True )
+
+    @classmethod
+    def makeDurationTag(cls , duration):
+        days = duration.days
+        seconds = duration.seconds
+                
+        hours = 1.0 * seconds / 3600
+        minutes = 1.0 * seconds / 60
+
+        if days > 0:
+            return "%dD" % days
+
+        if hours > 0:
+            return "%dH" % hours
+
+        if minutes > 0:
+            return "%dM" % minutes
+
+        if seconds > 0:
+            return "%dS" % seconds
+
+
+        
+
+    def save(self, *args, **kwargs):
+        if self.description is None:
+            self.description = "Timeseries plot of sensors on:%s" % self.device.id
+
+        if self.tag == "":
+            if self.duration is None:
+                self.tag = "DEV:%s" % self.device.id
+            else:
+                if self.duration.total_seconds( ) <= 0:
+                    raise ValueError("Duration must be positive")
+                
+                self.tag = "DEV:%s:%s" % (self.device.id , self.makeDurationTag( self.duration ))
+
+        super(DevicePlot , self).save( *args , **kwargs )
+
+
+
+    def updatePlot(self):
+        try:
+            func = lib.device_plot
+        except:
+            raise ValidationError("Import python callable:%s failed" % self.python_callable)
+
+        new_html = func( self.device , self.duration )
+        if new_html:
+            self.html_code = new_html
+            self.last_update = datetime.datetime.now( pytz.utc )
+            super(Plot , self).save( )
+        else:
+            raise ValidationError("Python callable produced an empty string")
 
--- plot/tests/context.py
+++ plot/tests/context.py
@@ -1,4 +1,8 @@
 from pythoncall.models import *
+from git_version.models import * 
+from api_key.models import *
+from sensor.models import *
+
 
 def create_string( ):
     return "Plot"
@@ -18,4 +22,41 @@ class TestContext(object):
         self.plotly_call = PythonCall.objects.create( description = "PlotLy",
                                                      python_callable = "plot.lib.test")
         
+
+        self.git_version = GitVersion.objects.create( ref = "master" , repo = "uri://git.no" , description = "description")
+        self.key = ApiKey.objects.create( description = "Newkey")
+        self.external_key = str(self.key.external_key)
+        self.loc = Location.objects.create( name = "Ulriken" , latitude = 200 , longitude = 120 , altitude = 600)
+        self.dev_type = DeviceType.objects.create( name = "HP-X123" )
+        self.dev = Device.objects.create( id = "DevXYZ" , 
+                                          location = self.loc , 
+                                          device_type = self.dev_type , 
+                                          description = "Besrkivels",
+                                          post_key = self.key)
+
+        self.mtype = MeasurementType.objects.create( name = "Temperature" )
+        self.raw_data = DataType.objects.get( pk = "RAWDATA" )
+        self.test_data = DataType.objects.get( pk = "TEST" )
+
+        self.sensor_type_temp = SensorType.objects.create( product_name = "XX12762 Turbo",
+                                                           measurement_type = self.mtype,
+                                                           short_description = "Temp",
+                                                           description = "Measurement of temperature",
+                                                           unit = "Degree celcius",
+                                                           min_value = 0,
+                                                           max_value = 100)
         
+        self.temp_sensor = Sensor.objects.create( id = "TEMP:XX",
+                                                  parent_device = self.dev,
+                                                  description = "tempm",
+                                                  sensor_type = self.sensor_type_temp)
+
+        RawData.create( {"sensorid" : self.temp_sensor.id , 
+                         "value" : "10",
+                         "key" : self.key , 
+                         "timestamp" : "2015-10-10T10:10:00+01" })
+
+        RawData.create( {"sensorid" : self.temp_sensor.id , 
+                         "value" : "12",
+                         "key" : self.key , 
+                         "timestamp" : "2015-11-10T10:10:00+01" })

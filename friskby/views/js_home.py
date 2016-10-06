fe8e85c906adb1e1a86ceec49a61ae18267b93bc
Joakim Hove <joakim.hove@gmail.com>
2016-10-06 21:29:04 +0200

    Added missing module

---
+++ friskby/views/js_home.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+from plot.models import *
+
+class JSHome(View):
+    
+    def get(self , request):
+        context = { }
+        return render( request , "friskby/index.html" , context )

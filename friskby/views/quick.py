8ebf2a3e1ac5efe5f58ac9187f62b9a5e93fff79
Morten Bendiksen <morten.bendiksen@gmail.com>
2016-12-18 00:38:47 +0100

    Make a table. Draw a map (with no points yet)

--- friskby/views/quick.py
+++ friskby/views/quick.py
@@ -5,13 +5,42 @@ from plot.models import *
 
 import sensor.models as models
 
-class Quick(View):
-    
-    def get(self , request):
-        sensor_list = models.Sensor.objects.all()
-        
-        for s in sensor_list:
-            print vars(s)
+def get_current_timestamp():
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT CURRENT_TIMESTAMP")
+    rows = cursor.fetchone()
+    return rows[0]
 
-        context = {"sensor_list" : sensor_list }
+class Quick(View):
+
+    def get(self , request):
+        device_list = models.Device.objects.all()
+
+        current_time = request.GET.get('time', get_current_timestamp())
+
+        device_rows = []
+        for d in device_list:
+            pm25sensor = next(x for x in d.sensorList() if x.description == "PM25")
+            pm10sensor = next(x for x in d.sensorList() if x.description == "PM10")
+            dataSql = """
+            SELECT * FROM sensor_rawdata
+            where sensor_id = '%s' and timestamp_recieved < '%s'
+            order by timestamp_recieved asc LIMIT 1"""
+            data25 = models.RawData.objects.raw(dataSql % (pm25sensor.id, current_time))[0]
+            data10 = models.RawData.objects.raw(dataSql % (pm10sensor.id, current_time))[0]
+
+            row = {
+                'id': d.id,
+                'locname': d.location.name,
+                'lat': d.location.latitude,
+                'long': d.location.longitude,
+                'pm25': data25.value,
+                'pm10': data10.value,
+                'time': data25.timestamp_recieved }
+            device_rows.append(row)
+            print(vars(d))
+            print(row)
+
+        context = {"device_rows" : device_rows }
         return render( request , "friskby/quick.html" , context )

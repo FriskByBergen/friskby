3ddf95d143147d6a09632d45680c8d2a6297a215
Pål Grønås Drange <pgdr@users.noreply.github.com>
2017-02-13 22:26:23 +0100

    Added downsample tests (#131)
    Added downsample tests

--- friskby/tests/test_quick_view.py
+++ friskby/tests/test_quick_view.py
@@ -5,11 +5,34 @@ from rest_framework import status
 
 from .context import TestContext
 from sensor.models import *
+from friskby.views.quick import downsample
+from datetime import datetime as dt
 
 class QuickTest(TestCase):
     def setUp(self):
         self.context = TestContext( )
-        
+
+    def test_downsample_time(self):
+        def gen_date(minut):
+            return dt(2017,1,1,1,minut,1)
+        data = []
+        num_points = 53
+        for i in range(num_points):
+            data.append({'value': i, 'timestamp_data': gen_date(i)})
+        dsda = downsample(data, minutes=7, cutoff=41.0)
+        self.assertEqual(8, len(dsda))
+        self.assertEqual(gen_date(num_points-1), dsda[-1]['timestamp_data'])
+        self.assertEqual(41.0, dsda[-1]['value'])
+
+
+    def test_downsample_maxval(self):
+        self.assertEqual([], downsample([]))
+        maxval = 123.45
+        e = {'value': 150, 'timestamp_data': dt.now()}
+        x = downsample([e], cutoff=maxval)
+        e_ds = x[0]
+        self.assertEqual(1, len(x))
+        self.assertEqual(maxval, e_ds['value'])
         
     def test_get(self):
         url = reverse("friskby.view.quick")
--- friskby/views/quick.py
+++ friskby/views/quick.py
@@ -15,21 +15,24 @@ def make_timestamp( row ):
     row["timestamp_data"] = TimeStamp.create( row["timestamp_data"] )
     return row
 
+def with_cutoff(elt, cutoff):
+    elt['value'] = min(cutoff, elt['value'])
+    return elt
+
 def downsample(lst, minutes = 15, cutoff = 100):
     if not lst:
         return []
     ret = []
     prev = None
-    ret.append(lst[0])
+    ret.append(with_cutoff(lst[0], cutoff))
     prev = lst[0]['timestamp_data']
     for e in lst:
         delta = e['timestamp_data'] - prev
         if delta.total_seconds() > minutes*60:
-            e['value'] = min(cutoff, e['value'])
-            ret.append(e)
+            ret.append(with_cutoff(e, cutoff))
             prev = e['timestamp_data']
-    if ret[-1] != lst[-1]:
-        ret.append(lst[-1]) # retain the last element
+    if ret[-1]['timestamp_data'] != lst[-1]['timestamp_data']:
+        ret.append(with_cutoff(lst[-1], cutoff)) # retain the last element
     return ret
 
 class Quick(View):

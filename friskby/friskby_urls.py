76a76420355b8cf9b5337eed7c5a9653fb29959e
Pål Grønås Drange <pgdr@statoil.com>
2017-02-15 18:51:45 +0100

    Quick is promoted! Retain old as legacy

--- friskby/friskby_urls.py
+++ friskby/friskby_urls.py
@@ -1,5 +1,5 @@
-from   django.conf.urls import url, include
-        
+from django.conf.urls import url, include
+
 from views import Home
 from views import Adm
 from views import JSHome
@@ -7,7 +7,7 @@ from views import Quick
 
 urlpatterns = [
     url(r'^friskby/adm/$' , Adm.as_view(), name = "friskby.view.adm"),
-    url(r'quick/$'        , Quick.as_view(), name = "friskby.view.quick"),
     url(r'friskby/$'      , Home.as_view()),
-    url(r'^$'             , JSHome.as_view())
+    url(r'legacy/$'       , JSHome.as_view()),
+    url(r'^$'             , Quick.as_view(), name = "friskby.view.quick")
 ]

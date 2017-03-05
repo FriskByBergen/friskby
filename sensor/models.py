f771421c74c94590603fc104d2ecc14877be17d6
Pål Grønås Drange <pgdr@statoil.com>
2017-03-05 18:27:41 +0100

    Made s_id primary_key. Moved RawData down in file

--- sensor/models.py
+++ sensor/models.py
@@ -14,9 +14,195 @@ from api_key.models import ApiKey
 from git_version.models import GitVersion
 
 
+
+class MeasurementType( Model ):
+    name = CharField("Type of measurement" , max_length = 60)
+
+    def __unicode__(self):
+        return self.name
+
+
+
+class Location( Model ):
+    name = CharField("Location" , max_length = 60 )
+    latitude = FloatField("Latitude")
+    longitude = FloatField("Longitude")
+    altitude = FloatField("Altitude" , null = True)
+
+    def __unicode__(self):
+        return self.name
+
+
+
+class DeviceType( Model ):
+    name = CharField("Name of the device" , max_length = 60 )
+
+    def __unicode__(self):
+        return self.name
+
+
+
+class Device( Model ):
+    IDPattern = "[-_:a-zA-Z0-9]+"
+
+    id = CharField("Device ID",
+                   max_length=60,
+                   primary_key=True,
+                   validators=[RegexValidator(regex="^%s$" % IDPattern)])
+    location = ForeignKey( Location , null = True )
+    device_type = ForeignKey( DeviceType )
+    description = CharField("Description" , max_length = 256 )
+    post_key = ForeignKey( ApiKey )
+    client_version = CharField(max_length = 128 , blank = True , null = True)
+    git_version = ForeignKey( GitVersion , blank = True , null = True)
+    locked = BooleanField( default = True )
+    owner = ForeignKey( User )
+
+
+    def __unicode__(self):
+        return self.id
+
+    def valid_post_key( self , key_string):
+        return self.post_key.access( key_string )
+
+    def sensorList(self):
+        return Sensor.objects.filter( parent_device = self )
+
+    def clientConfig(self):
+        # The post key is not set here, and must be explicitly set in the
+        # view code if the request is correctly authorized.
+        config = {"sensor_list" : [ sensor.sensor_id for sensor in self.sensorList() ],
+                  "post_path" : reverse("sensor.api.post"),
+                  "config_path" : reverse("sensor.device_config" , args = [self.id]),
+                  "device_id" : self.id }
+
+        if self.git_version:
+            config["git_repo"] = self.git_version.repo
+            config["git_ref"] = self.git_version.ref
+            config["git_follow"] = self.git_version.follow_head
+
+        return config
+
+    def lockDevice(self):
+        self.locked = True
+        self.save( )
+
+
+
+class SensorType( Model ):
+    product_name = CharField( "Product name" , max_length = 256 )
+    short_description = CharField("Short description" , max_length = 40)
+    measurement_type = ForeignKey( MeasurementType )
+    description = CharField("Description" , max_length = 256 )
+    unit = CharField("Unit" , max_length = 60 )
+    min_value = FloatField("Minimum value")
+    max_value = FloatField("Maximum value")
+
+    def __unicode__(self):
+        return self.short_description
+
+
+    def valid_range(self , value):
+        if self.min_value <= value <= self.max_value:
+            return True
+        else:
+            return False
+
+
+    def valid_input(self , value):
+        if not isinstance(value,float):
+            try:
+                value = float(value)
+            except:
+                return False
+
+        return self.valid_range( value )
+
+
+
+class DataType( Model ):
+    id = CharField("DataType" , max_length = 60 , primary_key = True)
+
+    def __unicode__(self):
+        return self.id
+
+
+
+class Sensor( Model ):
+    IDPattern = "[-_:a-zA-Z0-9]+"
+
+    s_id = IntegerField(primary_key=True)
+    sensor_id = CharField("Sensor ID",
+                          max_length=60,
+                          validators=[RegexValidator(regex="^%s$" % IDPattern)])
+    sensor_type = ForeignKey( SensorType )
+    parent_device = ForeignKey( Device )
+    data_type = ForeignKey( DataType , default = "TEST" )
+    description = CharField("Description" , max_length = 256 )
+    on_line = BooleanField( default = True )
+    last_value = FloatField( null = True , blank = True)
+    last_timestamp = DateTimeField( null = True , blank = True)
+
+    def __unicode__(self):
+        return self.sensor_id
+
+
+    def valid_input(self , input_value):
+        return self.sensor_type.valid_input( input_value )
+
+
+
+    # Can speicify *either* a start or number of values with keyword
+    # arguments 'start' and 'num', but not both.
+    def get_ts(self, num = None , start = None , end = None):
+        return RawData.get_ts( self , num = num, start = start , end = end)
+
+
+    def get_vectors(self , num = None , start = None , end = None):
+        return RawData.get_vectors( self , num = num , start = start , end = end)
+
+
+    def get_current(self , timeout_seconds):
+        current = {}
+        data_value = DataValue.objects.select_related('data_info__timestamp').filter( data_info__sensor = self).order_by('data_info__timestamp__timestamp').last()
+        if data_value is None:
+            return None
+
+        ts = data_value.data_info.timestamp.timestamp
+        value = data_value.value
+        if timeout_seconds > 0:
+            if timezone.now() - ts > datetime.timedelta( seconds = timeout_seconds ):
+                value = None
+
+
+        location = data_value.data_info.location
+        return {"sensorid"  : self.sensor_id,
+                "timestamp" : data_value.data_info.timestamp.timestamp,
+                "value"     : value,
+                "location"  : {"latitude" : location.latitude , "longitude" : location.longitude}}
+
+
+    def valid_post_key( self , key_string):
+        return self.parent_device.valid_post_key( key_string )
+
+
+    # This method returns a QuerySet - because that query set is
+    # subsequently used to update the status of all the relevant
+    # RawData records.
+    def get_rawdata(self):
+        qs = RawData.objects.filter(sensor_id=self.sensor_id ,
+                                    processed=False).values_list('id',
+                                                                 'timestamp_data',
+                                                                 'value').order_by('timestamp_data')
+
+        return qs
+
+
+
 class RawData(Model):
     sensor_id = CharField(max_length=128)
-    timestamp_recieved = DateTimeField(  ) 
+    # s_id = ForeignKey(Sensor, null=True)
+    timestamp_recieved = DateTimeField(  )
     timestamp_data = DateTimeField( )
     value = FloatField( default = -1 )
     processed = BooleanField( default = False )
@@ -24,22 +210,22 @@ class RawData(Model):
     def __unicode__(self):
         return "Sensor:%s: Value:%s" % (self.sensor_id , self.value)
 
-    
+
     def save(self,*args, **kwargs):
         if self.timestamp_recieved is None:
             self.timestamp_recieved = datetime.datetime.now( pytz.utc )
         super(RawData , self).save(*args , **kwargs)
-        
 
 
 
 
-    
+
+
     @classmethod
     def error(cls , data ):
         if data is None:
             return "Error: empty payload"
-        
+
         missing_keys = []
         for key in ["key" , "sensorid" , "value" , "timestamp"]:
             if not key in data:
@@ -54,20 +240,20 @@ class RawData(Model):
         return None
 
     @classmethod
-    def get_ts(cls , sensor , num = None , start = None , end = None): 
+    def get_ts(cls , sensor , num = None , start = None , end = None):
         ts = []
         if num is None:
             if start is None and end is None:
                 qs = RawData.objects.filter( sensor_id = sensor.sensor_id ).order_by('timestamp_data')
             else:
                 if end is None:
-                    qs = RawData.objects.filter( sensor_id = sensor.sensor_id , 
+                    qs = RawData.objects.filter( sensor_id = sensor.sensor_id ,
                                                  timestamp_data__gte = start).order_by('timestamp_data')
                 elif start is None:
-                    qs = RawData.objects.filter( sensor_id = sensor.sensor_id , 
+                    qs = RawData.objects.filter( sensor_id = sensor.sensor_id ,
                                                  timestamp_data__lte = end).order_by('timestamp_data')
                 else:
-                    qs = RawData.objects.filter( sensor_id = sensor.sensor_id , 
+                    qs = RawData.objects.filter( sensor_id = sensor.sensor_id ,
                                                  timestamp_data__range = [start,end]).order_by('timestamp_data')
 
         else:
@@ -75,7 +261,7 @@ class RawData(Model):
                 qs = reversed( RawData.objects.filter( sensor_id = sensor.sensor_id ).order_by('-timestamp_data')[:num] )
             else:
                 raise ValueError("Can not supply both num and start")
-            
+
         for entry in qs:
             ts.append( (entry.timestamp_data , entry.value) )
         return ts
@@ -103,7 +289,7 @@ class RawData(Model):
                 raise ValueError("No such sensor: %s" % sensor_id)
         else:
             raise ValueError("Must have 'sensorid' as part of data")
-        
+
         if "key" in data:
             key = data["key"]
             if not sensor.valid_post_key( key ):
@@ -137,93 +323,19 @@ class RawData(Model):
                 values.append( value )
         except:
             raise ValueError("Invalid data")
-            
+
         rawdata = []
         for ts,value in zip(timestamp,values):
             rd = RawData( sensor_id = sensor_id,
                           timestamp_data = ts,
                           value = value )
-            
-                    
+
+
             rd.save()
             rawdata.append( rd )
 
         return rawdata
 
-        
-
-class Location( Model ):
-    name = CharField("Location" , max_length = 60 )
-    latitude = FloatField("Latitude")
-    longitude = FloatField("Longitude")
-    altitude = FloatField("Altitude" , null = True)
-
-    def __unicode__(self):
-        return self.name
-
-
-class DataType( Model ):
-    id = CharField("DataType" , max_length = 60 , primary_key = True)
-
-    def __unicode__(self):
-        return self.id
-
-
-class DeviceType( Model ):
-    name = CharField("Name of the device" , max_length = 60 )
-    
-    def __unicode__(self):
-        return self.name
-
-
-class Device( Model ):
-    IDPattern = "[-_:a-zA-Z0-9]+"
-
-    id = CharField("Device ID" , max_length = 60 , primary_key = True , validators = [RegexValidator(regex = "^%s$" % IDPattern)])
-    location = ForeignKey( Location , null = True )
-    device_type = ForeignKey( DeviceType )
-    description = CharField("Description" , max_length = 256 )
-    post_key = ForeignKey( ApiKey )
-    client_version = CharField(max_length = 128 , blank = True , null = True)
-    git_version = ForeignKey( GitVersion , blank = True , null = True)
-    locked = BooleanField( default = True )
-    owner = ForeignKey( User )
-
-
-    def __unicode__(self):
-        return self.id
-
-    def valid_post_key( self , key_string):
-        return self.post_key.access( key_string )
-
-    def sensorList(self):
-        return Sensor.objects.filter( parent_device = self )
-    
-    def clientConfig(self):
-        # The post key is not set here, and must be explicitly set in the 
-        # view code if the request is correctly authorized.
-        config = {"sensor_list" : [ sensor.sensor_id for sensor in self.sensorList() ],
-                  "post_path" : reverse("sensor.api.post"),
-                  "config_path" : reverse("sensor.device_config" , args = [self.id]),
-                  "device_id" : self.id }
-        
-        if self.git_version:
-            config["git_repo"] = self.git_version.repo
-            config["git_ref"] = self.git_version.ref
-            config["git_follow"] = self.git_version.follow_head
-        
-        return config
-
-    def lockDevice(self):
-        self.locked = True
-        self.save( )
-
-
-class MeasurementType( Model ):
-    name = CharField("Type of measurement" , max_length = 60)
-    
-    def __unicode__(self):
-        return self.name
 
 
 class TimeStamp( Model ):
@@ -256,108 +368,9 @@ class TimeStamp( Model ):
 
 
 
-class SensorType( Model ):
-    product_name = CharField( "Product name" , max_length = 256 )
-    short_description = CharField("Short description" , max_length = 40)
-    measurement_type = ForeignKey( MeasurementType )
-    description = CharField("Description" , max_length = 256 )
-    unit = CharField("Unit" , max_length = 60 )
-    min_value = FloatField("Minimum value")
-    max_value = FloatField("Maximum value")
-
-    def __unicode__(self):
-        return self.short_description
-
-
-    def valid_range(self , value):
-        if self.min_value <= value <= self.max_value:
-            return True
-        else:
-            return False
-
-
-    def valid_input(self , value):
-        if not isinstance(value,float):
-            try:
-                value = float(value)
-            except:
-                return False
-            
-        return self.valid_range( value )
-
-
-
-class Sensor( Model ):
-    IDPattern = "[-_:a-zA-Z0-9]+"
-
-    s_id = IntegerField()
-    sensor_id = CharField("Sensor ID" , max_length = 60 , primary_key = True , validators = [RegexValidator(regex = "^%s$" % IDPattern)])
-    sensor_type = ForeignKey( SensorType )
-    parent_device = ForeignKey( Device )
-    data_type = ForeignKey( DataType , default = "TEST" )
-    description = CharField("Description" , max_length = 256 )
-    on_line = BooleanField( default = True )
-    last_value = FloatField( null = True , blank = True)
-    last_timestamp = DateTimeField( null = True , blank = True) 
-
-    def __unicode__(self):
-        return self.sensor_id
-        
-
-    def valid_input(self , input_value):
-        return self.sensor_type.valid_input( input_value )
-
-
-
-    # Can speicify *either* a start or number of values with keyword
-    # arguments 'start' and 'num', but not both. 
-    def get_ts(self, num = None , start = None , end = None):
-        return RawData.get_ts( self , num = num, start = start , end = end)
-
-
-    def get_vectors(self , num = None , start = None , end = None):
-        return RawData.get_vectors( self , num = num , start = start , end = end)
-        
-
-    def get_current(self , timeout_seconds):
-        current = {}
-        data_value = DataValue.objects.select_related('data_info__timestamp').filter( data_info__sensor = self).order_by('data_info__timestamp__timestamp').last()
-        if data_value is None:
-            return None
-            
-        ts = data_value.data_info.timestamp.timestamp
-        value = data_value.value
-        if timeout_seconds > 0:
-            if timezone.now() - ts > datetime.timedelta( seconds = timeout_seconds ):
-                value = None
-                
-
-        location = data_value.data_info.location
-        return {"sensorid"  : self.sensor_id,
-                "timestamp" : data_value.data_info.timestamp.timestamp,
-                "value"     : value,
-                "location"  : {"latitude" : location.latitude , "longitude" : location.longitude}}
-
-
-    def valid_post_key( self , key_string):
-        return self.parent_device.valid_post_key( key_string )
-
-
-    # This method returns a QuerySet - because that query set is
-    # subsequently used to update the status of all the relevant
-    # RawData records.
-    def get_rawdata(self):
-        qs = RawData.objects.filter( sensor_id = self.sensor_id , 
-                                     processed = False).values_list( 'id', 'timestamp_data' , 'value').order_by('timestamp_data')
-        
-        return qs
-
-
-
-
 class ClientLog( Model ):
     device = ForeignKey( Device )
-    timestamp = DateTimeField( ) 
+    timestamp = DateTimeField( )
     msg = CharField( max_length = 256 )
     long_msg = TextField( blank = True, null = True )
 

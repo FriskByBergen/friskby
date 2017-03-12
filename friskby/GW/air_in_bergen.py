d1e2ff621e178f80b1d16b8530c41953af0b2483
Pål Grønås Drange <pgdr@statoil.com>
2017-03-12 20:58:01 +0100

    refactored to __name__==__main__ pattern

--- friskby/GW/air_in_bergen.py
+++ friskby/GW/air_in_bergen.py
@@ -5,34 +5,15 @@ import pytz
 import pandas as pd
 from friskby_gw import FriskByGW
 
-sensor_types = ['_PM10', '_PM25']
-
-GW = FriskByGW()
-
-base_url = 'http://www.luftkvalitet.info/home/overview.aspx?type=2&topic=1&id=%'
-key_bg1 = '7b751808f5-d561-4737-9185-4ecc0e834975%7d' # LODDEFJORD
-key_bg2 = '7b4ff685c1-ad51-4468-b2fc-08345d11f447%7d' # DANMARKSPLASS
-key_bg3 = '7b5b0ff070-e6e6-4f60-88a3-bd923ac3a7e6%7d' # RAADHUSET
-key_bg4 = '7bceade2ac-e62f-4e50-af7c-347e402fff27%7d' # AASANE
-
-luft_map = {"BG_1": '%s%s' % (base_url, key_bg1),
-            "BG_2": '%s%s' % (base_url, key_bg2),
-            "BG_3": '%s%s' % (base_url, key_bg3),
-            "BG_4": '%s%s' % (base_url, key_bg4)}
-
-# keys for parsing data
-komponent_id = ["PM10", "PM2.5"]
-
-key = "9fbb2727-067d-4618-af05-e45cafdd673d"
-
-bergen = pytz.timezone('Europe/Oslo')
-bergen_time = dt.now(bergen)
-
-# probably this function is not necessary and we can just use 0, 1, 2 in post_data(device, df) instead of these variables,
-# but if something will change in table, f.e. the first value will be Date, then Time etc. we'll have some
-# unexpected values and the script will crash
 def data_index(df):
-    # we need just the first value of 'Verdi', 'cause the second is from yesterday measurings
+    """Probably this function is not necessary and we can just use 0, 1, 2 in
+    post_data(device, df) instead of these variables, but if something will
+    change in table, e.g. the first value will be Date, then Time etc. we'll
+    have some unexpected values and the script will crash.
+
+    """
+    # we need just the first value of 'Verdi', since the second is from
+    # yesterday's measurings
     counter = 0
     for i in range(len(df)):
         for j in range(len(df[i])):
@@ -46,7 +27,11 @@ def data_index(df):
     return component, tid, value
 
 
-def post_data(device, df):
+def post_data(GW, device, df, bergen_time):
+    sensor_types = ['_PM10', '_PM25']
+    komponent_id = ["PM10", "PM2.5"]
+    key = "9fbb2727-067d-4618-af05-e45cafdd673d"
+
     component, tid, value = data_index(df)
     for dev_id in range(len(komponent_id)):
         for i in range(len(df)):
@@ -62,10 +47,30 @@ def post_data(device, df):
                 if(n >= 0):
                     sensor.postValue(n, timestamp = ts)
 
-for device in luft_map:
-    try:
-        url_read = pd.read_html(luft_map[device], header=0, thousands=".")[1]
-        df = url_read.values.tolist()
-        post_data(device, df)
-    except ValueError as e:
-        pass
+
+def main():
+    GW = FriskByGW()
+    base_url = 'http://www.luftkvalitet.info/home/overview.aspx?type=2&topic=1&id=%'
+    key_bg1 = '7b751808f5-d561-4737-9185-4ecc0e834975%7d' # LODDEFJORD
+    key_bg2 = '7b4ff685c1-ad51-4468-b2fc-08345d11f447%7d' # DANMARKSPLASS
+    key_bg3 = '7b5b0ff070-e6e6-4f60-88a3-bd923ac3a7e6%7d' # RAADHUSET
+    key_bg4 = '7bceade2ac-e62f-4e50-af7c-347e402fff27%7d' # AASANE
+
+    luft_map = {"BG_1": '%s%s' % (base_url, key_bg1),
+                "BG_2": '%s%s' % (base_url, key_bg2),
+                "BG_3": '%s%s' % (base_url, key_bg3),
+                "BG_4": '%s%s' % (base_url, key_bg4)}
+
+    bergen_tz = pytz.timezone('Europe/Oslo')
+    bergen_time = dt.now(bergen_tz)
+
+    for device in luft_map:
+        try:
+            url_read = pd.read_html(luft_map[device], header=0, thousands=".")[1]
+            df = url_read.values.tolist()
+            post_data(GW, device, df, bergen_time)
+        except ValueError as err:
+            print('Error occurred on device = %s: %s.' % (device, err))
+
+if __name__ == '__main__':
+    main()
